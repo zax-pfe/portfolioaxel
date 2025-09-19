da47a157b3d7668cf361dcc02c1ef7eb9e76c57b
zax-pfe <axel75puech@gmail.com>
2025-09-19 15:44:41 +0200

    ajout description et changement icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,8 +12,32 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Axel Puech",
-  description: "My Portfolio",
+  title: "Axel Puech - Web Developer & AI Enthusiast | Portfolio",
+  description:
+    "Explore the portfolio of Axel Puech — showcasing projects in web development, artificial intelligence, and creative design.",
+
+  keywords: [
+    "Axel Puech",
+    "Web Developer",
+    "AI Projects",
+    "Portfolio",
+    "Frontend Development",
+    "Fullstack Developer",
+    "Creative Design",
+  ],
+  authors: [{ name: "Axel Puech" }],
+
+  openGraph: {
+    title: "Axel Puech - Web Developer & AI Enthusiast | Portfolio",
+    description:
+      "Explore the portfolio of Axel Puech — showcasing projects in web development, artificial intelligence, and creative design.",
+    url: "https://www.axelpuech.com",
+    siteName: "Axel Puech Portfolio",
+
+    locale: "en_US",
+    type: "website",
+  },
+
   icons: {
     icon: "/assets/icon/favicon.png",
   },
